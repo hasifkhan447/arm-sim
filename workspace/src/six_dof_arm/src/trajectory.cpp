126694fe9640afb4e427f7a5d7cfe87127c5f531
hasifkhan447 <hasifkhan.447@gmail.com>
2025-05-13 22:20:27 +0500

    Successfully moving to end trajectory

--- workspace/src/six_dof_arm/src/trajectory.cpp
+++ workspace/src/six_dof_arm/src/trajectory.cpp
@@ -15,10 +15,41 @@ int main(int argc, char * argv[])
   // Create a ROS logger
   auto const logger = rclcpp::get_logger("hello_moveit");
 
-  // Next step goes here
+  // Create the MoveIt MoveGroup Interface
+  using moveit::planning_interface::MoveGroupInterface;
+  auto move_group_interface = MoveGroupInterface(node, "arm_group");
+
+  // Set a target Pose
+  auto const target_pose = []{
+    geometry_msgs::msg::Pose msg;
+    msg.orientation.w = 1.0;
+    msg.position.x = 1;
+    msg.position.y = 0;
+    msg.position.z = 0.3;
+    return msg;
+  }();
+  move_group_interface.setPoseTarget(target_pose);
+
+  // Create a plan to that target pose
+  auto const [success, plan] = [&move_group_interface]{
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
+    return std::make_pair(ok, msg);
+  }();
+
+  // Execute the plan
+  if(success) {
+    move_group_interface.execute(plan);
+  } else {
+    RCLCPP_ERROR(logger, "Planing failed!");
+  }
+
 
   // Shutdown ROS
   rclcpp::shutdown();
   return 0;
 }
+
+
+
 
