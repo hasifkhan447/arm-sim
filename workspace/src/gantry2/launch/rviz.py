3e2a28db3240a828b98a529cd39447658e257366
hak <hasifkhan447@gmail.com>
2025-09-10 14:25:30 +0500

    Just added in the model

---
+++ workspace/src/gantry2/launch/rviz.py
@@ -0,0 +1,60 @@
+import os 
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+
+from launch_ros.actions import Node 
+import xacro 
+
+def generate_launch_description():
+    package_path = os.path.join(
+        get_package_share_directory('gantry2')
+    )
+
+    xacro_path = os.path.join(package_path,
+                              'urdf',
+                              'gantry.xacro')
+
+    rviz_config_path = os.path.join(package_path,
+                                    'rviz',
+                                    'view_only.rviz')
+
+    xacro_doc = xacro.parse(open(xacro_path))
+    xacro.process_doc(xacro_doc)
+
+    params = {'robot_description' : xacro_doc.toxml()}
+
+    node_world_publisher = Node(
+        package='tf',
+        executable='static_transform_publisher',
+    )
+
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    rviz_node_launch = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=['-d', rviz_config_path],
+        parameters=[
+            {'use_sim_time' : True},
+        ]
+    )
+
+
+
+    return LaunchDescription([
+        node_robot_state_publisher,
+        rviz_node_launch,
+    ])
+
+
+
