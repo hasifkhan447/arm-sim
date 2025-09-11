a17801c68bddbd18498317e99a48e1d2393a948d
hak <hasifkhan447@gmail.com>
2025-09-11 14:52:46 +0500

    Working one railing

--- workspace/src/gantry2/launch/rviz.py
+++ workspace/src/gantry2/launch/rviz.py
@@ -59,7 +59,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         node_robot_state_publisher,
-        # node_joint_state_publisher_gui,
+        node_joint_state_publisher_gui,
         node_joint_state_publisher,
         rviz_node_launch,
     ])
