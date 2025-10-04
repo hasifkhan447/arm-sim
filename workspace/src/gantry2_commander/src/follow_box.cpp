d6d18d498c9c33e1f7baad472d9c8909ba52e89a
Hamad Asif Khan <hasifkhan.447@gmail.com>
2025-10-04 14:38:54 +0500

    Added velocity interface

--- workspace/src/gantry2_commander/src/follow_box.cpp
+++ workspace/src/gantry2_commander/src/follow_box.cpp
@@ -17,7 +17,6 @@ static const rclcpp::Logger LOGGER = rclcpp::get_logger("moveit2_tutorials.servo
 // First we declare pointers to the node and publisher that will publish commands to Servo
 rclcpp::Node::SharedPtr node_;
 rclcpp::Publisher<control_msgs::msg::JointJog>::SharedPtr joint_cmd_pub_;
-rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_cmd_pub_;
 size_t count_ = 0;
 
 // BEGIN_SUB_TUTORIAL publishCommands
@@ -28,27 +27,17 @@ void publishCommands()
   // First we will publish 100 joint jogging commands. The :code:`joint_names` field allows you to specify individual
   // joints to move, at the velocity in the corresponding :code:`velocities` field. It is important that the message
   // contains a recent timestamp, or Servo will think the command is stale and will not move the robot.
-  if (count_ < 100)
-  {
-    auto msg = std::make_unique<control_msgs::msg::JointJog>();
-    msg->header.stamp = node_->now();
-    msg->joint_names.push_back("xaxis");
-    msg->velocities.push_back(0.3);
-    joint_cmd_pub_->publish(std::move(msg));
-    ++count_;
-  }
+  auto msg = std::make_unique<control_msgs::msg::JointJog>();
+  msg->header.stamp = node_->now();
+  msg->joint_names.push_back("xaxis");
+  msg->joint_names.push_back("yaxis");
+  msg->joint_names.push_back("zaxis");
+  msg->velocities.push_back(0.03 + count_/100);
+  msg->velocities.push_back(0.04 + count_/100);
+  msg->velocities.push_back(0.04);
+  joint_cmd_pub_->publish(std::move(msg));
+  ++count_;
 
-  // After a while, we switch to publishing twist commands. The provided frame is the frame in which the twist is
-  // defined, not the robot frame that will follow the command. Again, we need a recent timestamp in the message
-  else
-  {
-    auto msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
-    msg->header.stamp = node_->now();
-    msg->header.frame_id = "xaxis";
-    msg->twist.linear.x = 0.3;
-    msg->twist.angular.z = 0.5;
-    twist_cmd_pub_->publish(std::move(msg));
-  }
 }
 // END_SUB_TUTORIAL
 
@@ -91,38 +80,6 @@ int main(int argc, char** argv)
   // messages which will directly jog the robot in the joint space, and TwistStamped messages which will move the
   // specified link with the commanded Cartesian velocity. In this demo, we jog the end effector link.
   joint_cmd_pub_ = node_->create_publisher<control_msgs::msg::JointJog>("servo_demo_node/delta_joint_cmds", 10);
-  twist_cmd_pub_ = node_->create_publisher<geometry_msgs::msg::TwistStamped>("servo_demo_node/delta_twist_cmds", 10);
-
-  // Next we will create a collision object in the way of the arm. As the arm is servoed towards it, it will slow down
-  // and stop before colliding
-  moveit_msgs::msg::CollisionObject collision_object;
-  collision_object.header.frame_id = "zaxis";
-  collision_object.id = "cardboard_box";
-
-  // Make a box and put it in the way
-  shape_msgs::msg::SolidPrimitive box;
-  box.type = box.BOX;
-  box.dimensions = { 0.1, 0.4, 0.1 };
-  geometry_msgs::msg::Pose box_pose;
-  box_pose.position.x = 0.6;
-  box_pose.position.y = 0.0;
-  box_pose.position.z = 0.6;
-
-  // Add the box as a collision object
-  collision_object.primitives.push_back(box);
-  collision_object.primitive_poses.push_back(box_pose);
-  collision_object.operation = collision_object.ADD;
-
-  // Create the message to publish the collision object
-  moveit_msgs::msg::PlanningSceneWorld psw;
-  psw.collision_objects.push_back(collision_object);
-  moveit_msgs::msg::PlanningScene ps;
-  ps.is_diff = true;
-  ps.world = psw;
-
-  // Publish the collision object to the planning scene
-  auto scene_pub = node_->create_publisher<moveit_msgs::msg::PlanningScene>("planning_scene", 10);
-  scene_pub->publish(ps);
 
   // Initializing Servo
   // ^^^^^^^^^^^^^^^^^^
